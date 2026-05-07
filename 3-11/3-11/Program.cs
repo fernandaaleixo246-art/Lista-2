c69fe2d2bdf4684b88af9e488670bdd3f892eb60
fernandaaleixo246-art <fernandaaleixo246@gmail.com>
2026-05-07 17:14:08 -0300

    Add files via upload

---
+++ 3-11/3-11/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+
+            Console.Write("Digite o primeiro valor: ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o segundo valor: ");
+            b = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o terceiro valor: ");
+            c = int.Parse(Console.ReadLine());
+
+            int maior = a;
+
+            if (b > maior)
+            {
+                maior = b;
+            }
+
+            if (c > maior)
+            {
+                maior = c;
+            }
+
+            Console.WriteLine("Maior valor: " + maior);
+
+            Console.ReadKey();
+        }
+    }
+}
