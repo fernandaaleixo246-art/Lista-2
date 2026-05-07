4336fbcdeffa0701186e82f7c11939ce19d8fc4c
fernandaaleixo246-art <fernandaaleixo246@gmail.com>
2026-05-07 17:16:09 -0300

    Add files via upload

---
+++ 4-11/4-11/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double baseRet, altura, area;
+
+            Console.Write("Digite a base: ");
+            baseRet = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite a altura: ");
+            altura = double.Parse(Console.ReadLine());
+
+            area = baseRet * altura;
+
+            Console.WriteLine("Área: " + area);
+
+            if (area > 100)
+            {
+                Console.WriteLine("Terreno grande");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
