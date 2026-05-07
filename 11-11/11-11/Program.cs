309a25d53dcdab71db4c43518ca74892432d2c1d
fernandaaleixo246-art <fernandaaleixo246@gmail.com>
2026-05-07 17:29:03 -0300

    Add files via upload

---
+++ 11-11/11-11/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double p1, p2;
+
+            Console.Write("Digite a nota da P1: ");
+            p1 = double.Parse(Console.ReadLine());
+
+            p2 = (15 - p1) / 2;
+
+            Console.WriteLine("Você precisa tirar " + p2 + " na P2 para ser aprovado.");
+
+            Console.ReadKey();
+        }
+    }
+}
