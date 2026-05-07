09cbd58af6093745dd5fc67cd5c00a8a68829679
fernandaaleixo246-art <fernandaaleixo246@gmail.com>
2026-05-07 17:21:47 -0300

    Add files via upload

---
+++ 7-11/7-11/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+
+            Console.Write("Digite o lado A: ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o lado B: ");
+            b = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o lado C: ");
+            c = int.Parse(Console.ReadLine());
+
+            if (a < b + c && b < a + c && c < a + b)
+            {
+                if (a == b && b == c)
+                {
+                    Console.WriteLine("Triângulo Equilátero");
+                }
+                else if (a == b || a == c || b == c)
+                {
+                    Console.WriteLine("Triângulo Isósceles");
+                }
+                else
+                {
+                    Console.WriteLine("Triângulo Escaleno");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Não forma triângulo");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
